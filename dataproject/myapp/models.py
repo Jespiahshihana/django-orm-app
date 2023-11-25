d18f8151129a477da303b114c5fb63a078b25bfb
Jespiahshihana <jespiahshihana@gmail.com>
2023-11-25 13:32:46 +0530

    added

--- dataproject/myapp/models.py
+++ dataproject/myapp/models.py
@@ -1,21 +1,3 @@
-# Django ORM Web Application
-
-## AIM
-To develop a Django application to store and retrieve data from a database using Object Relational Mapping(ORM).
-
-
-## DESIGN STEPS
-
-### STEP 1:
-Create a new exercise
-### STEP 2:
-Enter the code appopriately
-### STEP 3:
-Run the program
-
-
-## PROGRAM
-```
 from django.db import models
 from django.contrib import admin
 # Create your models here.
@@ -29,11 +11,3 @@ class Student (models.Model):
 class StudentAdmin(admin.ModelAdmin):
     list_display=('referencenumber','name','age','email','mobileno')
 
-```
-## OUTPUT
-![output](./orm.png)
-
-
-
-## RESULT
-Thus the django orm created successfully 
